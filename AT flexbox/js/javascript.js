d858d17361f549da88e1f53e88b619a50c246bdf
viniciustvs <94203171+viniciustvs@users.noreply.github.com>
2022-01-25 21:55:31 -0300

    Add files via upload

---
+++ AT flexbox/js/javascript.js
@@ -0,0 +1,9 @@
+// let baixo = document.getElementsByClassName("baixo");
+
+// function mouseOver(){
+//     baixo.style.background = "black";
+// }
+
+// function mouseOut(){
+//     baixo.style.backgound = "#a1c3ff"
+// }
